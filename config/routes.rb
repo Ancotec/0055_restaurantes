da935736a23c172a05342484fe785d976e9b65fc
Ancotec <leandro.vidal@ancotec.cl>
2021-10-20 20:06:10 -0300

    actualizados al lunes 18102021

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tipos_comidas', to: 'tipos_comidas#listar', as: 'tipos_comidas' #listar
+  get 'tipos_comidas/nuevo', to: 'tipos_comidas#crear', as: 'nuevo_tipo_comida' #formulario de nuevo
+  get 'tipos_comidas/:id', to: 'tipos_comidas#mostrar', as: 'tipo_comida' #vista del detalle de un tipo
+  get 'tipos_comidas/:id/editar', to: 'tipos_comidas#editar', as:'editar_tipo_comida' # formulario para editar el
+
+  post 'tipos_comidas', to: 'tipos_comidas#guardar'
+
+  delete 'tipos_comidas/:id', to: 'tipos_comidas#eliminar'
+
+  
+
 end
